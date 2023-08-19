0c8bd771688ad28ee9ecf7ff7e4cb56a57d74fb0
olitreadwell <oliver.treadwell@gmail.com>
2023-08-19 00:03:15 -0700

    feat: begin NavBar component

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Articles</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
