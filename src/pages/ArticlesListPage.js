92b7bc0c388a5052e4cd28f701840b93b0eefb11
olitreadwell <oliver.treadwell@gmail.com>
2023-08-18 23:45:18 -0700

    feat: create simple layout for pages

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,10 @@
+const ArticlesListPage = () => {
+  return (
+    <div>
+      <h1>Articles List Page</h1>
+      <p>This is the Articles List page</p>
+    </div>
+  );
+}
+
+export default ArticlesListPage;
