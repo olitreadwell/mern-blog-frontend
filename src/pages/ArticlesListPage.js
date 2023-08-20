a17eb7d66559e7465b8b867dee96b51aaf90e23b
olitreadwell <oliver.treadwell@gmail.com>
2023-08-20 16:43:55 -0700

    feat: link to the article page from article list page

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
+import articles from "./article-content";
+
 const ArticlesListPage = () => {
   return (
     <div>
       <h1>Articles List Page</h1>
-      <p>This is the Articles List page</p>
+      {articles.map((article) => (
+        <Link
+          className="article-list-item"
+          to={`/articles/${article.name}`}
+          key={article.name}>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)} ...</p>
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
 export default ArticlesListPage;
