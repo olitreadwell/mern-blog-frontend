827ede7263dc5a6764d5d55816da505b2d4cc4fb
olitreadwell <oliver.treadwell@gmail.com>
2023-08-20 16:50:19 -0700

    chore: use React fragments instead of divs

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,9 @@
 const AboutPage = () => {
   return (
-    <div>
+    <>
       <h1>About Page</h1>
       <p>This is the about page</p>
-    </div>
+    </>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,9 @@
 const HomePage = () => {
   return (
-    <div>
+    <>
       <h1>Home Page</h1>
       <p>This is the home page</p>
-    </div>
+    </>
   );
 }
 
