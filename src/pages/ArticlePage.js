24a8efe8dae0202df8536d81a3776a17dd6afa45
olitreadwell <oliver.treadwell@gmail.com>
2023-08-21 21:57:57 -0700

    feat: display upvotes from server

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,20 +1,42 @@
+// external imports
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
+// internal imports
 import NotFoundPage from "./NotFoundPage";
 
 // Data
 import articles from "./article-content";
 
 const ArticlePage = () => {
+  // get the articleId from the URL
   const { articleId } = useParams();
+  
+  // set the articleInfo state to an object with upvotes and comments
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  
+  
+  useEffect(() => {
+    const loadArticleInfo = async (articleId) => {
+      const response = await axios.get(`/api/v1/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+
+    loadArticleInfo(articleId);
+
+  }, [articleId]);
+  
 
   const article = articles.find(article => article.name === articleId);
-
+ 
   if (!article) return (<NotFoundPage />);
   
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
