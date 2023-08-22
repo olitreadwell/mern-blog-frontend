033f67d348547c6a5601bca1add41222ac55cf1b
olitreadwell <oliver.treadwell@gmail.com>
2023-08-22 00:03:53 -0700

    config: add firebase config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVv_SLbAl8Vx2UXZFVwsdPEZYXRyVr5vs",
+  authDomain: "linkedin-blog-a59c2.firebaseapp.com",
+  projectId: "linkedin-blog-a59c2",
+  storageBucket: "linkedin-blog-a59c2.appspot.com",
+  messagingSenderId: "813195691770",
+  appId: "1:813195691770:web:6908c027feface0b05ab43"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
