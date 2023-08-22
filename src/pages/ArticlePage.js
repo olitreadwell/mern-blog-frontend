5941abc454684fbee00e99039e628a924d0c7d8f
olitreadwell <oliver.treadwell@gmail.com>
2023-08-21 22:22:18 -0700

    feat: add upvote button & null display for comments

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -11,39 +11,54 @@ import CommentsList from "../components/CommentsList";
 import articles from "./article-content";
 
 const ArticlePage = () => {
-  // get the articleId from the URL
-  const { articleId } = useParams();
-  
-  // set the articleInfo state to an object with upvotes and comments
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-  
-  
-  useEffect(() => {
-    const loadArticleInfo = async (articleId) => {
-      const response = await axios.get(`/api/v1/articles/${articleId}`);
-      const newArticleInfo = response.data;
-      setArticleInfo(newArticleInfo);
+    // get the articleId from the URL
+    const { articleId } = useParams();
+
+    // set the articleInfo state to an object with upvotes and comments
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+
+    useEffect(() => {
+        const loadArticleInfo = async (articleId) => {
+            const response = await axios.get(`/api/v1/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
+
+        loadArticleInfo(articleId);
+    }, [articleId]);
+
+    const article = articles.find((article) => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(
+            `/api/v1/articles/${articleId}/upvote`
+        );
+        const updatedArticleInfo = response.data;
+        setArticleInfo(updatedArticleInfo);
     };
 
-    loadArticleInfo(articleId);
+    if (!article) return <NotFoundPage />;
 
-  }, [articleId]);
-  
-
-  const article = articles.find(article => article.name === articleId);
- 
-  if (!article) return (<NotFoundPage />);
-  
-  return (
-    <>
-      <h1>{article.title}</h1>
-      <p>This post has been upvoted {articleInfo.upvotes} times</p>
-      {article.content.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))}
-      <CommentsList comments={articleInfo.comments} />
-    </>
-  );
-}
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={() => addUpvote()}>Add Upvote</button>
+                <p>This post has been upvoted {articleInfo.upvotes} times</p>
+            </div>
+            {article.content.map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+            ))}
+            {articleInfo.comments.length > 0 ? (
+                <CommentsList comments={articleInfo.comments} />
+            ) : (
+                <p>No comments yet.</p>
+            )}
+        </>
+    );
+};
 
 export default ArticlePage;
